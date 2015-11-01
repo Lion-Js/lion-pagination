982838760693f939a7d9a30310895dd0d7bb2f60
小龙 <rainbolean@sina.com>
2015-11-01 23:09:08 +0800

    添加gulp任务

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,37 @@
 (function(){
     'use strict';
     var gulp = require('gulp');
+    var compass = require('gulp-compass');
+    var plumber = require('gulp-plumber');
+    var concat = require('gulp-concat');
+    var rename = require('gulp-rename');
+    var minify_css = require('gulp-minify-css');
 
-    gulp.task('test', function() {
-        console.log(222);
-    });
+    //编译sass文件
+    gulp.task('compile_sass', function() {
+		return gulp.src('./src/sass/pages/*.scss')
+			.pipe(plumber())
+			.pipe(compass({
+			    css: './build/css',
+      		    sass: './src/sass/pages'
+			}))
+			.pipe(minify_css())
+		    .pipe(rename(function(path) {
+		        path.basename += '.min';
+		    }))
+		    .pipe(gulp.dest('./build/css'));
+	});
 
-    gulp.task('build', [ 'test' ]);
+    //watch
+	gulp.task('watch_sass', function() {
+	    return gulp.watch('./src/sass/**/*', ['compile_sass']);
+	});
+
+	//发布版本
+	gulp.task('publish', function() {
+        return gulp.src('./build/**/*').pipe(gulp.dest('./dist/'));
+	});
+
+    gulp.task('build', ['compile_sass']);
+    gulp.task('watch', ['watch_sass']);
 })();
